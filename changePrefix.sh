c9223e6751615a57986892373892282f832e426a
Ebi Soroush <ebrahim.soroush@gmail.com>
2017-06-27 10:55:27 +0430

    adds changePrefix

--- changePrefix.sh
+++ changePrefix.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 PREFIX=$1
 echo $PREFIX
-find -iname Makefile | while read a; do sed -i 's|PREFIX =*|PREFIX = $PREFIX|g' $a; done
+find -iname "Makefile*" | while read a; do sed -i "s|PREFIX =.*|PREFIX = $PREFIX|g" $a; done
